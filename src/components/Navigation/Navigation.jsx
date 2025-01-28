cbe0f7667a8495438b9aecb5a584947b5dcce783
Denis Gordeev <povar572349@gmail.com>
2025-01-28 16:19:59 +0200

    chages for PR

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,24 +1,36 @@
 import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
 import './Navigation.scss';
 import logo from '../../../logo.png'
+
 const Navigation = () => {
-    return (
-      <header className="header">
-        <NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}>
+  return (
+    <header className="header">
+      <NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}>
         <img src={logo} alt='Logo' />
+      </NavLink>
+      <nav className="nav">
+        <NavLink to="/" className={({ isActive }) => cn({ "active-link": isActive })}>
+          Головна
         </NavLink>
-        <nav className="nav">
-          <NavLink to="/" className={({ isActive }) => isActive ? "active-link" : ""}>Головна</NavLink>
-          <NavLink to="/tournaments" className={({ isActive }) => isActive ? "active-link" : ""}>Турніри</NavLink>
-          <NavLink to="/teams" className={({ isActive }) => isActive ? "active-link" : ""}>Команди</NavLink>
-          <NavLink to="/players" className={({ isActive }) => isActive ? "active-link" : ""}>Гравці</NavLink>
-          <NavLink to="/rating" className={({ isActive }) => isActive ? "active-link" : ""}>Рейтинг</NavLink>
-        </nav>
-        <NavLink to="/login" className={({ isActive }) => isActive ? "active-link" : ""}>
+        <NavLink to="/tournaments" className={({ isActive }) => cn({ "active-link": isActive })}>
+          Турніри
+        </NavLink>
+        <NavLink to="/teams" className={({ isActive }) => cn({ "active-link": isActive })}>
+          Команди
+        </NavLink>
+        <NavLink to="/players" className={({ isActive }) => cn({ "active-link": isActive })}>
+          Гравці
+        </NavLink>
+        <NavLink to="/rating" className={({ isActive }) => cn({ "active-link": isActive })}>
+          Рейтинг
+        </NavLink>
+      </nav>
+      <NavLink to="/login" className={({ isActive }) => isActive ? "active-link" : ""}>
         <button className="login-btn">Login</button>
-        </NavLink>
-      </header>
-    );
-  };
-  
-  export default Navigation;
+      </NavLink>
+    </header>
+  );
+};
+
+export default Navigation;
